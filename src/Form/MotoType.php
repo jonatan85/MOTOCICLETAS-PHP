e6d2a544b395c981dff97b0371b25c09e7330d91
jonatan85 <114008152+jonatan85@users.noreply.github.com>
2023-03-10 21:47:54 +0100

    formularios-completos

--- src/Form/MotoType.php
+++ src/Form/MotoType.php
@@ -3,10 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Moto;
+use App\Entity\Tipo;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class MotoType extends AbstractType
 {
@@ -20,7 +22,17 @@ class MotoType extends AbstractType
             ->add('cilindrada')
             ->add('color')
             ->add('description')
-            // ->add('Tipos')
+            ->add('tipo', EntityType::class, [
+                // looks for choices from this entity
+                'class' => Tipo::class,
+            
+                // uses the User.username property as the visible option string
+                'choice_label' => 'nombre',
+            
+                // used to render a select box, check boxes or radios
+                'multiple' => true,
+                // 'expanded' => true,
+            ])
             ->add('enviar', SubmitType::class)
         ;
     }
